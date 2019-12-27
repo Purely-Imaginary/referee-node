3ab48eb84aabc0b71cd2e2c1d26f30020ec6556d
Amadeusz Opac <a.opac@dualmedia.pl>
2019-12-27 11:50:56 +0100

    adding sample secrets

---
+++ src/secrets.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+
+export const mongoUrl = 'mongodb://localhost:27017/referee';
+export const spreadsheetURL2 = 'https://docs.google.com/spreadsheets/d/1ZcanOKj8IMd4AEynruOyp0D5HIY05wUqEjg4P2Z6ang/export?format=csv&id=1ZcanOKj8IMd4AEynruOyp0D5HIY05wUqEjg4P2Z6ang&gid=0';
